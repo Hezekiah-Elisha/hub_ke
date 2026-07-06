26324b277a36faa5474761f6246223581641de90
Hezekiah Elisha <elishahezekiah903@gmail.com>
2026-07-06 13:33:55 +0300

    style: update Hero component layout and typography for improved presentation

--- components/layout/Hero.tsx
+++ components/layout/Hero.tsx
@@ -5,18 +5,18 @@ import { Button } from "../ui/button";
 export default function Hero() {
     return (
         <section className="h-screen relative flex md:flex-row flex-col justify-center items-center font-archivo">
-            <div className="flex-1 w-full px-10 flex md:flex-row flex-col justify-center align-middle pt-10 md:pt-0">
-                <Image
+            <div className="flex-1 w-full px-10 flex md:flex-row flex-col justify-center align-middle pt-10 md:pt-0 bg-[url('/hero-background.webp')] bg-cover bg-center shadow-shadow border-border h-screen">
+                {/* <Image
                     src={'/hero-background.webp'}
                     alt="Hero Background"
                     width={1500}
                     height={1200}
                     className="object-cover object-center shadow-shadow border-border w-full h-full"
                     loading="eager"
-                />
+                /> */}
             </div>
             <div className='flex-1 p-2 justify-center items-center md:items-start flex flex-col gap-4 text-center md:text-left'>
-                <h2 className='text-xl md:text-8xl font-bold text-main-foreground border-border shadow-shadow'>Hub KE</h2>
+                <h2 className='text-6xl md:text-8xl font-bold text-main-foreground border-border shadow-shadow'>Hub KE</h2>
                 <p>
                     Hub KE is a technology organization that specializes in
                     providing innovative solutions to businesses and
